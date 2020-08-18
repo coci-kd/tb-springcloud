5a59507479bece83630b3c453ec7543a10b87f45
coci-kd <1804834408@qq.com>
2020-08-18 19:19:44 +0800

    注释内容

---
+++ tb-feign/src/main/java/com/jk/service/UserServiceFeign.java
@@ -0,0 +1,8 @@
+package com.jk.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "tb-provider-two", fallback = UserServiceFallback.class)
+public interface UserServiceFeign extends UserService {
+
+}
